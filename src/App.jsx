9e33f8f35d2539206543dc83e5200e93ba738a3a
roma <kopitchak2006@gmail.com>
2025-11-04 12:31:26 +0200

    page 404

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import HomePage from './pages/HomePage.jsx';
+import PerfumeDetailPage from './pages/PerfumeDetail.jsx';
 import BrandsPage from './pages/BrandsPage.jsx';
+import FavoritesPage from './pages/FavoritesPage.jsx';
 import AboutPage from './pages/AboutPage.jsx';
 import AuthPage from './pages/AuthPage.jsx';
-import FavoritesPage from './pages/FavoritesPage.jsx';
-import PerfumeDetailPage from './pages/PerfumeDetail.jsx';
+import NotFoundPage from './pages/NotFoundPage.jsx';
 
 function App() {
   return (
@@ -18,7 +19,7 @@ function App() {
         <Route path="/auth" element={<AuthPage />} />
         <Route path="/favorites" element={<FavoritesPage />} />
         <Route path="/perfume/:id" element={<PerfumeDetailPage />} />
-        <Route path="*" element={<h1>404 | Сторінку не знайдено</h1>} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );
