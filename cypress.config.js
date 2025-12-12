f84b78836f96c19dfc23ee95cac151d8f3de8d66
roma <kopitchak2006@gmail.com>
2025-12-12 17:50:50 +0200

    FIX

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'cypress';
+
 import codeCoverageTask from '@cypress/code-coverage/task';
 
 export default defineConfig({
--- cypress/e2e/navigation.cy.js
+++ cypress/e2e/navigation.cy.js
@@ -1,10 +1,12 @@
 describe('Навігація по сайту (E2E Real)', () => {
+  const baseUrl = Cypress.config('baseUrl');
+
   beforeEach(() => {
     cy.visit('/');
   });
 
   it('успішно завантажує головну сторінку', () => {
-    cy.url().should('eq', 'http://localhost:5173/');
+    cy.url().should('eq', `${baseUrl}/`);
 
     cy.contains('ЗНАЙДИ САМЕ ТЕ').should('be.visible');
     cy.contains('НОВИНКИ').should('be.visible');
@@ -14,7 +16,6 @@ describe('Навігація по сайту (E2E Real)', () => {
     cy.get('nav').contains('Бренди').click();
 
     cy.url().should('include', '/brands');
-
     cy.contains('БРЕНДИ').should('be.visible');
     cy.contains('Популярні бренди').should('be.visible');
   });
@@ -23,7 +24,6 @@ describe('Навігація по сайту (E2E Real)', () => {
     cy.get('nav').contains('Про нас').click();
 
     cy.url().should('include', '/about');
-
     cy.contains('Про нас').should('be.visible');
     cy.contains('Наша Філософія').should('be.visible');
   });
@@ -34,17 +34,15 @@ describe('Навігація по сайту (E2E Real)', () => {
       .click();
 
     cy.url().should('include', '/auth');
-
     cy.contains('button', 'Реєстрація').should('be.visible');
     cy.get('input[type="email"]').should('be.visible');
   });
 
   it('клік на логотип повертає на головну', () => {
     cy.visit('/brands');
-
     cy.contains('a', 'Perfumer').click();
 
-    cy.url().should('eq', 'http://localhost:5173/');
+    cy.url().should('eq', `${baseUrl}/`);
     cy.contains('ЗНАЙДИ САМЕ ТЕ').should('be.visible');
   });
 });
